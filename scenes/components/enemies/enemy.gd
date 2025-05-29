b0b8f822e57fac67b7a73233c4ab8b7d65369f1c
ztamagoru <zdamaria119@gmail.com>
2025-05-29 12:41:07 -0300

    fixed save manager bug and now enemy flips h on movement
    fixed bug: error when player doesn't have the save file, now creates new save file with default values and double jump deactivated

--- scenes/components/enemies/enemy.gd
+++ scenes/components/enemies/enemy.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 class_name Enemy
 
 @export var hp : int
+@export var sprite : Sprite2D
 
 func take_damage(damage : int):
 	hp -= damage
@@ -17,6 +18,15 @@ func blink():
 	await get_tree().create_timer(0.1).timeout
 	self.modulate.a = 100
 	await get_tree().create_timer(0.1).timeout
+
+func _process(_delta):
+	sprite_flip()
+
+func sprite_flip():
+	if velocity.x < 0:
+		sprite.flip_h = true
+	elif velocity.x > 0:
+		sprite.flip_h = false
 
 func die():
 	queue_free()
--- scenes/components/save manager/save_manager.gd
+++ scenes/components/save manager/save_manager.gd
@@ -7,23 +7,42 @@ var save_data : Dictionary = {
 	, "double_jump" : true
 	}
 
+var route : String = "user://save_file.save"
+
 func _ready():
 	Globals.save_manager = self
 	load_game()
 
 func save_game():
-	save_data.hp = Globals.player.hp
-	save_data.mana = Globals.player.mana
-	save_data.coins = Globals.player.coins
-	save_data.double_jump = Globals.player.double_jump
+	if not FileAccess.file_exists(route):
+		set_default_save_data()
+		print("new save file created")
+	else:
+		set_current_player_data()
 	
-	var save_file = FileAccess.open("user://save_file.save", FileAccess.WRITE)
+	var save_file = FileAccess.open(route, FileAccess.WRITE)
 	save_file.store_line(JSON.stringify(save_data))
 
 func load_game():
-	var save_file = FileAccess.open("user://save_file.save", FileAccess.READ)
+	if not FileAccess.file_exists(route):
+		save_game()
+		return
+	
+	var save_file = FileAccess.open(route, FileAccess.READ)
 	var save_file_text = save_file.get_line()
 	var json = JSON.new()
 	
 	json.parse(save_file_text)
 	save_data = json.data
+
+func set_default_save_data():
+	save_data.hp = Globals.player.hp_max
+	save_data.mana = Globals.player.mana_max
+	save_data.coins = 0
+	save_data.double_jump = false
+
+func set_current_player_data():
+	save_data.hp = Globals.player.hp
+	save_data.mana = Globals.player.mana
+	save_data.coins = Globals.player.coins
+	save_data.double_jump = Globals.player.double_jump
